1275740cfde674db675207a76e6bdd88e3e4dbd3
Chris Hudson <chudson@amelco.co.uk>
2017-02-03 15:33:07 +0000

    Attempting to loop through plugins and install them

---
+++ roles/jenkins/files/config_scripts/basic_security.groovy
@@ -0,0 +1,19 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+def env = System.getenv()
+def adminPassword = env['JENKINS_ADMIN_PASSWORD']
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('admin',adminPassword)
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
